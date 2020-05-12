b10051c79a7b5fd7116459759dd7d9601e5b90f0
jolleen <jleverich2000@gmail.com>
2020-05-12 17:21:29 -0400

    started recipe saving

--- src/app/save-recipe/save-recipe.component.ts
+++ src/app/save-recipe/save-recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'; 
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-save-recipe',
@@ -6,17 +7,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./save-recipe.component.css']
 })
 export class SaveRecipeComponent implements OnInit {
-
+  saveRecipeForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onAddIngredient(){
-
+    this.initForm();
   }
 
   onSubmit(){
-    
+    console.log(this.saveRecipeForm)
   }
+
+  onAddIngredient() {
+    (<FormArray>this.saveRecipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    );
+  }
+
+  onAddSteps() {
+    (<FormArray>this.saveRecipeForm.get('directions')).push(
+      new FormGroup({
+        'step': new FormControl(),
+        'order': new FormControl()
+      })
+    );
+
 }
+
+  get controls() {
+    return (<FormArray>this.saveRecipeForm.get('ingredients')).controls;
+  }
+  get stepControls() {
+    return (<FormArray>this.saveRecipeForm.get('directions')).controls;
+  }
+  
+  private initForm() {
+      let recipeName = "";
+       let Ingredients = new FormArray([]);
+       let Directions = new FormArray([]);
+      let category = "";
+
+      this.saveRecipeForm = new FormGroup({
+        'recipeName': new FormControl(recipeName),
+        'ingredients': Ingredients,
+        'directions' : Directions,
+        'category': new FormControl(category)
+      });
+    }
+    
+
+}
+
